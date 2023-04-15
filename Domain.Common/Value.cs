162f298bc8b5f9cef36716ceef1e2686a2d2b72b
SteffenSkaras <steffen.bs@outlook.com>
2023-04-15 09:18:30 +0200

    added entity and value classes

---
+++ Domain.Common/Value.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Metadata;
+
+namespace Domain.Common
+{
+    public class Value<T, TObject>
+    {
+        public TObject Property { get; private set; }
+
+        public Value(TObject property)
+        {
+            Property = property;
+        }
+    }
+}
