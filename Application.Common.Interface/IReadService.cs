6bdd407c596132baf6ec5affc4e2cf0205780f48
SteffenSkaras <steffen.bs@outlook.com>
2023-07-24 11:47:54 +0200

    added IReadService

---
+++ Application.Common.Interface/IReadService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Interface
+{
+    /// <summary>
+    /// Used to get dtos based on given query
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReadService<T, Q>
+    {
+        Task<T> ReadAsync(Q query);
+    }
+   
+    /// <summary>
+    /// Return dto of T
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IReadService<T>
+    {
+        Task<T> ReadAsync();
+    }
+}
